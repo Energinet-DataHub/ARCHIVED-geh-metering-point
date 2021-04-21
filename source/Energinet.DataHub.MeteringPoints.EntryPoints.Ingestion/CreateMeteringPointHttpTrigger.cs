f20697b96a46f747b6f55a8210e754e59b06380f
Christopher Maigaard <xchma@energinet.dk>
2021-04-21 10:58:56 +0200

    change driver

---
+++ source/Energinet.DataHub.MeteringPoints.EntryPoints.Ingestion/CreateMeteringPointHttpTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Energinet.DataHub.MeteringPoints.EntryPoints.Ingestion
+{
+    public static class HttpTrigger
+    {
+        [Function("CreateMeteringPoint")]
+        public static HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData request,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("CreateMeteringPointHttpTrigger");
+            logger.LogInformation("Received CreateMeteringPoint request");
+
+            var response = request.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Ready, set, go!");
+
+            return response;
+        }
+    }
+}
---
+++ source/Energinet.DataHub.MeteringPoints.EntryPoints.Ingestion/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Energinet.DataHub.MeteringPoints.EntryPoints.Ingestion
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
