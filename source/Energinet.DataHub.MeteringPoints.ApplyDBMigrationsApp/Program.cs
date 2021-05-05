3457de80d9261e9abad1f068d32109d6de22df0e
Daniel Pethjen <daniel@pethjen.dk>
2021-05-05 10:36:07 +0200

    Added DBMigrationsApp

---
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp.Helpers;
+
+namespace Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            var connectionString = ConnectionStringFactory.GetConnectionString(args);
+            var filter = EnvironmentFilter.GetFilter(args);
+            var isDryRun = args.Contains("dryRun");
+
+            var upgrader = UpgradeFactory.GetUpgradeEngine(connectionString, filter, isDryRun);
+
+            var result = upgrader.PerformUpgrade();
+
+            return ResultReporter.ReportResult(result);
+        }
+    }
+}
