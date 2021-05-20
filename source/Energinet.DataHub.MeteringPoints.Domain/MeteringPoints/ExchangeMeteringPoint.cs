b96adaed3bd3899f8bc8432437546ead9dad90eb
Thomas Bach Andersen <mail@thomasbach.dk>
2021-05-20 17:14:45 +0200

    Added inherited Metering Point types for Consumption, Exchange and Production

---
+++ source/Energinet.DataHub.MeteringPoints.Domain/MeteringPoints/ExchangeMeteringPoint.cs
@@ -0,0 +1,126 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using NodaTime;
+
+namespace Energinet.DataHub.MeteringPoints.Domain.MeteringPoints
+{
+    public class ExchangeMeteringPoint : MeteringPoint
+    {
+        public ExchangeMeteringPoint(
+            GsrnNumber gsrnNumber,
+            string streetName,
+            string postCode,
+            string cityName,
+            string countryCode,
+            bool isAddressWashable,
+            string physicalStatusOfMeteringPoint,
+            string meteringPointSubType,
+            string typeOfMeteringPoint,
+            string meteringGridArea,
+            string powerPlant,
+            string locationDescription,
+            string productType,
+            string parentRelatedMeteringPoint,
+            string unitType,
+            string meterNumber,
+            Instant? meterReadingOccurrence,
+            int maximumCurrent,
+            int maximumPower,
+            Instant? occurenceDate,
+            string toGrid,
+            string fromGrid)
+            : base(
+                gsrnNumber,
+                streetName,
+                postCode,
+                cityName,
+                countryCode,
+                isAddressWashable,
+                physicalStatusOfMeteringPoint,
+                meteringPointSubType,
+                typeOfMeteringPoint,
+                meteringGridArea,
+                powerPlant,
+                locationDescription,
+                productType,
+                parentRelatedMeteringPoint,
+                unitType,
+                meterNumber,
+                meterReadingOccurrence,
+                maximumCurrent,
+                maximumPower,
+                occurenceDate)
+        {
+            ToGrid = toGrid;
+            FromGrid = fromGrid;
+        }
+
+        public ExchangeMeteringPoint(
+            GsrnNumber gsrnNumber,
+            Guid id,
+            string streetName,
+            string postCode,
+            string cityName,
+            string countryCode,
+            bool isAddressWashable,
+            string physicalStatusOfMeteringPoint,
+            string meteringPointSubType,
+            string typeOfMeteringPoint,
+            string meteringGridArea,
+            string powerPlant,
+            string locationDescription,
+            string productType,
+            string parentRelatedMeteringPoint,
+            string unitType,
+            string meterNumber,
+            Instant? meterReadingOccurence,
+            int maximumCurrent,
+            int maximumPower,
+            Instant? occurenceDate,
+            string toGrid,
+            string fromGrid)
+            : base(
+                gsrnNumber,
+                id,
+                streetName,
+                postCode,
+                cityName,
+                countryCode,
+                isAddressWashable,
+                physicalStatusOfMeteringPoint,
+                meteringPointSubType,
+                typeOfMeteringPoint,
+                meteringGridArea,
+                powerPlant,
+                locationDescription,
+                productType,
+                parentRelatedMeteringPoint,
+                unitType,
+                meterNumber,
+                meterReadingOccurence,
+                maximumCurrent,
+                maximumPower,
+                occurenceDate)
+        {
+            ToGrid = toGrid;
+            FromGrid = fromGrid;
+        }
+
+        public string FromGrid { get; }
+
+        public string ToGrid { get; }
+    }
+}
