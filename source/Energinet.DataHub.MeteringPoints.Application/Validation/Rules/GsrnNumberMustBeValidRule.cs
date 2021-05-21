5601a1776f1ae7a4d56fab759616fc250f5cddc9
Karsten Marcussen <xnetsrak@gmail.com>
2021-05-21 12:57:48 +0200

    removed unused usings

--- source/Energinet.DataHub.MeteringPoints.Application/Validation/Rules/GsrnNumberMustBeValidRule.cs
+++ source/Energinet.DataHub.MeteringPoints.Application/Validation/Rules/GsrnNumberMustBeValidRule.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.MeteringPoints.Domain;
-using Energinet.DataHub.MeteringPoints.Domain.MeteringPoints;
 using Energinet.DataHub.MeteringPoints.Domain.SeedWork;
 using FluentValidation;
 
