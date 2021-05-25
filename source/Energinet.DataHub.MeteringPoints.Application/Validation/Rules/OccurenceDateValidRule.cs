2bf763a203f4052a9a7c459ee01478da72a7a31d
Karsten Marcussen <xnetsrak@gmail.com>
2021-05-25 10:10:05 +0200

    removed unused using

--- source/Energinet.DataHub.MeteringPoints.Application/Validation/Rules/OccurenceDateValidRule.cs
+++ source/Energinet.DataHub.MeteringPoints.Application/Validation/Rules/OccurenceDateValidRule.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Text.RegularExpressions;
 using FluentValidation;
 
 namespace Energinet.DataHub.MeteringPoints.Application.Validation.Rules
