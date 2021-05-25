cdd2b18f36013131180af1e4a283d9f3ca6b2358
Christopher Maigaard <xchma@energinet.dk>
2021-05-25 13:54:31 +0200

    Cleanup database was commented out

--- source/Energinet.DataHub.MeteringPoints.IntegrationTests/TestHost.cs
+++ source/Energinet.DataHub.MeteringPoints.IntegrationTests/TestHost.cs
@@ -92,7 +92,7 @@ namespace Energinet.DataHub.MeteringPoints.IntegrationTests
 
             _scope = AsyncScopedLifestyle.BeginScope(_container);
 
-            // CleanupDatabase();
+            CleanupDatabase();
         }
 
         private string ConnectionString =>
@@ -111,7 +111,7 @@ namespace Energinet.DataHub.MeteringPoints.IntegrationTests
                 return;
             }
 
-            // CleanupDatabase();
+            CleanupDatabase();
             _scope.Dispose();
             ((ServiceProvider)_serviceProvider).Dispose();
             _container.Dispose();
