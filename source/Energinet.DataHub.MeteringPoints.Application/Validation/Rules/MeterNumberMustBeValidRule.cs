db809af765de63ff686c33feee32856d7631013b
Karsten Marcussen <xnetsrak@gmail.com>
2021-05-31 08:34:17 +0200

    added validation for meternumber and Metering point sub type

---
+++ source/Energinet.DataHub.MeteringPoints.Application/Validation/Rules/MeterNumberMustBeValidRule.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Energinet.DataHub.MeteringPoints.Application.Validation.ValidationErrors;
+using Energinet.DataHub.MeteringPoints.Domain.MeteringPoints;
+using FluentValidation;
+
+namespace Energinet.DataHub.MeteringPoints.Application.Validation.Rules
+{
+    public class MeterNumberMustBeValidRule : AbstractValidator<CreateMeteringPoint>
+    {
+        private const int MeterNumberMaximumLength = 10;
+
+        public MeterNumberMustBeValidRule()
+        {
+            When(MeteringPointSubTypeIsPhysical, () =>
+            {
+                RuleFor(createMeteringPoint => createMeteringPoint.MeterNumber)
+                    .NotEmpty()
+                    .WithState(createMeteringPoint => new MeterNumberMandatoryValidationError(createMeteringPoint.GsrnNumber));
+
+                RuleFor(createMeteringPoint => createMeteringPoint.MeterNumber)
+                    .MaximumLength(MeterNumberMaximumLength)
+                    .WithState(createMeteringPoint => new MeterNumberMaximumLengthValidationError(createMeteringPoint.GsrnNumber));
+            }).Otherwise(() =>
+            {
+                RuleFor(createMeteringPoint => createMeteringPoint.MeterNumber)
+                    .NotEmpty()
+                    .WithState(createMeteringPoint => new MeterNumberNotAllowedValidationError(createMeteringPoint.GsrnNumber));
+            });
+        }
+
+        private static bool MeteringPointSubTypeIsPhysical(CreateMeteringPoint createMeteringPoint)
+        {
+            return createMeteringPoint.SubTypeOfMeteringPoint.Equals(MeteringPointSubType.Physical.Name, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
