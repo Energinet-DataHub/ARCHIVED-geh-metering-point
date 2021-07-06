a5c46798c74aa871a9d8201324b1f243791d37f2
Kristian F. Thomsen <kft@energinet.dk>
2021-07-06 10:03:00 +0200

    Add incoming message registry to handle message idempotency

---
+++ source/Energinet.DataHub.MeteringPoints.Infrastructure/Messaging/Idempotency/IncomingMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Energinet.DataHub.MeteringPoints.Infrastructure.Messaging.Idempotency
+{
+    public class IncomingMessage
+    {
+        public IncomingMessage(string messageId, string messageType)
+        {
+            MessageId = messageId;
+            MessageType = messageType;
+        }
+
+        public string MessageId { get; }
+
+        public string MessageType { get; }
+    }
+}
