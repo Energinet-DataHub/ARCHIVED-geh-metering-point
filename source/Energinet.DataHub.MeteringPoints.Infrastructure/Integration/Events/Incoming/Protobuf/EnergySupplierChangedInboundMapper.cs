a2f834c4dda39f33be1958c48c4600a91f24d33f
Kristian F. Thomsen <kft@energinet.dk>
2021-07-07 11:58:35 +0200

    Add contract and event handler for EnergySupplierChanged integration event, originating from market roles

---
+++ source/Energinet.DataHub.MeteringPoints.Infrastructure/Integration/Events/Incoming/Protobuf/EnergySupplierChangedInboundMapper.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.MeteringPoints.Application.Common.Transport;
+using Energinet.DataHub.MeteringPoints.Application.Connect;
+using Energinet.DataHub.MeteringPoints.Infrastructure.Transport.Protobuf;
+
+namespace Energinet.DataHub.MeteringPoints.Infrastructure.Integration.Events.Incoming.Protobuf
+{
+    public class EnergySupplierChangedInboundMapper : ProtobufInboundMapper<MarketRoles.IntegrationEventContracts.EnergySupplierChanged>
+    {
+        protected override IInboundMessage Convert(MarketRoles.IntegrationEventContracts.EnergySupplierChanged obj)
+        {
+            return new EnergySupplierChanged()
+            {
+                GsrnNumber = obj.GsrnNumber,
+            };
+        }
+    }
+}
