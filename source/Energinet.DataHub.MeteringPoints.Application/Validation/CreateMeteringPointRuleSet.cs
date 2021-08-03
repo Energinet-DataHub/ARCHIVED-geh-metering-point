1794f6af666124392f0872aaa22689ff21c3ff60
Daniel Pethjen <daniel@pethjen.dk>
2021-08-03 11:41:24 +0200

    Power plant validations part 1

--- source/Energinet.DataHub.MeteringPoints.Application/Validation/CreateMeteringPointRuleSet.cs
+++ source/Energinet.DataHub.MeteringPoints.Application/Validation/CreateMeteringPointRuleSet.cs
@@ -30,6 +30,7 @@ namespace Energinet.DataHub.MeteringPoints.Application.Validation
             RuleFor(request => request).SetValidator(new MeteringPointSubTypeMustBeValidRule());
             RuleFor(request => request).SetValidator(new MeterNumberMustBeValidRule());
             RuleFor(request => request).SetValidator(new LocationDescriptionMustBeValidRule());
+            RuleFor(request => request).SetValidator(new PowerPlantMustBeValidRule());
         }
     }
 }
