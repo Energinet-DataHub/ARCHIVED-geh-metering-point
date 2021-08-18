95e934add78ffbc42137ac56862a8ea06bbccdf3
Daniel Pethjen <daniel@pethjen.dk>
2021-08-18 15:04:37 +0200

    Update

--- source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Model/202108171611 Change columns for connection state.sql
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Model/202108171611 Change columns for connection state.sql
@@ -2,10 +2,14 @@ ALTER TABLE [dbo].[MeteringPoints]
     DROP COLUMN PhysicalStatusOfMeteringPoint
 GO
 ALTER TABLE [dbo].[MeteringPoints]
-ADD 
-    ConnectionState_PhysicalState NVARCHAR(255) NOT NULL CONSTRAINT DF_MeteringPoints_ConnectionState_PhysicalState DEFAULT 'defaultValue',
+ADD
+    ConnectionState_PhysicalState NVARCHAR(255) NULL,
     ConnectionState_EffectiveDate DATETIME2(7) NULL;
 GO
+
+UPDATE [dbo].[MeteringPoints]
+SET ConnectionState_PhysicalState = 'D03'
+
 ALTER TABLE [dbo].[MeteringPoints]
-DROP CONSTRAINT DF_MeteringPoints_ConnectionState_PhysicalState
+ALTER COLUMN ConnectionState_PhysicalState NVARCHAR(255) NOT NULL;
 GO
