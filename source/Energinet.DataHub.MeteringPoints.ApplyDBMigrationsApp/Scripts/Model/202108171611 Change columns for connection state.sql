bb0c961651473e0a7da1f66935268473c36e23c3
Daniel Pethjen <daniel@pethjen.dk>
2021-08-18 14:35:07 +0200

    Update Alter Table statement

--- source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Model/202108171611 Change columns for connection state.sql
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Model/202108171611 Change columns for connection state.sql
@@ -3,6 +3,9 @@ ALTER TABLE [dbo].[MeteringPoints]
 GO
 ALTER TABLE [dbo].[MeteringPoints]
 ADD 
-    ConnectionState_PhysicalState NVARCHAR(255) NOT NULL,
+    ConnectionState_PhysicalState NVARCHAR(255) NOT NULL CONSTRAINT DF_MeteringPoints_ConnectionState_PhysicalState DEFAULT 'defaultValue',
     ConnectionState_EffectiveDate DATETIME2(7) NULL;
+GO
+ALTER TABLE [dbo].[MeteringPoints]
+DROP CONSTRAINT DF_MeteringPoints_ConnectionState_PhysicalState
 GO
