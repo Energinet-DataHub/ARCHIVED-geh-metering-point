1d8a45960e6d0dcc53e6743aed8ac4f87670145e
Christopher Maigaard <xchma@energinet.dk>
2021-09-22 09:15:23 +0200

    add skeleton for grid areas

---
+++ source/Energinet.DataHub.MeteringPoints.Application/GridAreas/Create/CreateGridAreaHandler.cs
@@ -0,0 +1,90 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.MeteringPoints.Application.Common;
+using Energinet.DataHub.MeteringPoints.Domain.GridAreas;
+using Energinet.DataHub.MeteringPoints.Domain.SeedWork;
+
+namespace Energinet.DataHub.MeteringPoints.Application.GridAreas.Create
+{
+    public class CreateGridAreaHandler : IBusinessRequestHandler<CreateGridArea>
+    {
+        private IGridAreaRepository _gridAreaRepository;
+
+        public CreateGridAreaHandler(
+            IGridAreaRepository gridAreaRepository)
+        {
+            _gridAreaRepository = gridAreaRepository;
+        }
+
+        public async Task<BusinessProcessResult> Handle(CreateGridArea request, CancellationToken cancellationToken)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            var validationResult = await ValidateInputAsync(request, cancellationToken).ConfigureAwait(false);
+            if (!validationResult.Success)
+            {
+                return validationResult;
+            }
+
+            var gridAreaDetails = CreateDetails(request);
+
+            var businessRulesValidationResult = CheckBusinessRules(request, gridAreaDetails);
+            if (!businessRulesValidationResult.Success)
+            {
+                return businessRulesValidationResult;
+            }
+
+            var gridArea = GridArea.Create(gridAreaDetails);
+
+            _gridAreaRepository.Add(gridArea);
+
+            return BusinessProcessResult.Ok(request.TransactionId);
+        }
+
+        private static BusinessProcessResult CheckBusinessRules(CreateGridArea request, GridAreaDetails gridAreaDetails)
+        {
+            var validationResult = GridArea.CanCreate(gridAreaDetails);
+
+            return new BusinessProcessResult(request.TransactionId, validationResult.Errors);
+        }
+
+        private static async Task<BusinessProcessResult> ValidateInputAsync(CreateGridArea request, CancellationToken cancellationToken)
+        {
+            // TODO: implement
+            await Task.Delay(0, cancellationToken).ConfigureAwait(false);
+
+            var validationRules = new List<IBusinessRule>
+            {
+            };
+
+            return new BusinessProcessResult(request.TransactionId, validationRules);
+        }
+
+        private static GridAreaDetails CreateDetails(CreateGridArea request)
+        {
+            // TODO: convert to value objects.
+            return new GridAreaDetails(
+                request.Name!,
+                request.Code!,
+                request.OperatorName!,
+                request.OperatorId!,
+                request.PriceAreaCode!);
+        }
+    }
+}
---
+++ source/Energinet.DataHub.MeteringPoints.Domain/GridAreas/GridArea.cs
@@ -0,0 +1,75 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.ObjectModel;
+using Energinet.DataHub.MeteringPoints.Domain.SeedWork;
+
+namespace Energinet.DataHub.MeteringPoints.Domain.GridAreas
+{
+    public class GridArea
+    {
+        private readonly string _name;
+        private readonly string _code;
+        private readonly string _operatorName;
+        private readonly string _operatorId;
+        private readonly string _priceAreaCode;
+
+        private GridArea(
+            string name,
+            string code,
+            string operatorName,
+            string operatorId,
+            string priceAreaCode)
+        {
+            _name = name;
+            _code = code;
+            _operatorName = operatorName;
+            _operatorId = operatorId;
+            _priceAreaCode = priceAreaCode;
+        }
+
+#pragma warning disable 8618 // Must have an empty constructor, since EF cannot bind Address in main constructor
+        private GridArea() { }
+#pragma warning restore 8618
+
+        public static BusinessRulesValidationResult CanCreate(GridAreaDetails gridAreaDetails)
+        {
+            if (gridAreaDetails == null) throw new ArgumentNullException(nameof(gridAreaDetails));
+
+            var rules = new Collection<IBusinessRule>()
+            {
+            };
+
+            return new BusinessRulesValidationResult(rules);
+        }
+
+        public static GridArea Create(GridAreaDetails gridAreaDetails)
+        {
+            if (gridAreaDetails == null) throw new ArgumentNullException(nameof(gridAreaDetails));
+
+            if (!CanCreate(gridAreaDetails).Success)
+            {
+                throw new InvalidOperationException("Cannot create grid area due to violation of one or more business rules.");
+            }
+
+            return new GridArea(
+                gridAreaDetails.Name,
+                gridAreaDetails.Code,
+                gridAreaDetails.OperatorName,
+                gridAreaDetails.OperatorId,
+                gridAreaDetails.PriceAreaCode);
+        }
+    }
+}
