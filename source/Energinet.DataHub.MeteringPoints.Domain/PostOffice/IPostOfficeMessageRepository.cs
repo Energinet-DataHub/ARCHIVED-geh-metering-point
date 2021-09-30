22f187cc405a37586d70e2f0035da01128cee9df
Thomas Bach Andersen <xtban@energinet.dk>
2021-09-30 09:56:57 +0200

    Added entity and repository for PostOfficeMessageMetadata

---
+++ source/Energinet.DataHub.MeteringPoints.Domain/PostOffice/IPostOfficeMessageRepository.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Energinet.DataHub.MeteringPoints.Domain.PostOffice
+{
+    /// <summary>
+    /// Repository for market facing metering points
+    /// </summary>
+    public interface IPostOfficeMessageMetadataRepository
+    {
+        /// <summary>
+        /// Fetch Post Office Message Metadata by id
+        /// </summary>
+        Task<PostOfficeMessageMetadata> GetMessageAsync(Guid messageId);
+
+        /// <summary>
+        /// Fetch Post Office Message Metadata by id
+        /// </summary>
+        Task<PostOfficeMessageMetadata[]> GetMessagesAsync(Guid[] messageIds);
+
+        /// <summary>
+        /// Save Post Office Message Metadata
+        /// </summary>
+        Task SaveMessageAsync(PostOfficeMessageMetadata postOfficeMessageMetadata);
+    }
+}
