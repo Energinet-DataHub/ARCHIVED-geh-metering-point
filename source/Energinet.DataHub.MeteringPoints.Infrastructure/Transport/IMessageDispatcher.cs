ad3e71b9b511cb4b7784b4193fad51ee6efb94c7
Thomas Bach Andersen <xtban@energinet.dk>
2021-09-30 09:50:51 +0200

    Add interface for MessageDispatcher

--- source/Energinet.DataHub.MeteringPoints.Infrastructure/Transport/IMessageDispatcher.cs
+++ source/Energinet.DataHub.MeteringPoints.Infrastructure/Transport/IMessageDispatcher.cs
@@ -22,34 +22,14 @@ namespace Energinet.DataHub.MeteringPoints.Infrastructure.Transport
     /// <summary>
     /// A class that combines the serialized format, and the means of transport
     /// </summary>
-    public class MessageDispatcher : IMessageDispatcher
+    public interface IMessageDispatcher
     {
-        private readonly MessageSerializer _serializer;
-        private readonly Channel _channel;
-
-        /// <summary>
-        /// Construct a <see cref="MessageDispatcher"/>
-        /// </summary>
-        /// <param name="serializer">Serializer to use</param>
-        /// <param name="channel">The channel where the data is sent to</param>
-        public MessageDispatcher(MessageSerializer serializer, Channel channel)
-        {
-            _serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
-            _channel = channel ?? throw new ArgumentNullException(nameof(channel));
-        }
-
         /// <summary>
         /// Send a <paramref name="message"/> to a channel
         /// </summary>
         /// <param name="message">Message to send</param>
         /// <param name="cancellationToken">Cancellation token for the operation</param>
         /// <exception cref="ArgumentNullException">message is <c>null</c></exception>
-        public async Task DispatchAsync(IOutboundMessage message, CancellationToken cancellationToken = default)
-        {
-            if (message == null) throw new ArgumentNullException(nameof(message));
-            var data = await _serializer.ToBytesAsync(message, cancellationToken).ConfigureAwait(false);
-
-            await _channel.WriteToAsync(data, cancellationToken).ConfigureAwait(false);
-        }
+        public Task DispatchAsync(IOutboundMessage message, CancellationToken cancellationToken = default);
     }
 }
