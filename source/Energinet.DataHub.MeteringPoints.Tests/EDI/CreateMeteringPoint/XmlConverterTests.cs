93b6ab6832d1a2dd995ced96f7ee7ad6cac39256
Thomas Bach Andersen <xtban@energinet.dk>
2021-10-12 10:33:01 +0200

    Add support for nullable types in translator func in xml converter

--- source/Energinet.DataHub.MeteringPoints.Tests/EDI/CreateMeteringPoint/XmlConverterTests.cs
+++ source/Energinet.DataHub.MeteringPoints.Tests/EDI/CreateMeteringPoint/XmlConverterTests.cs
@@ -107,7 +107,7 @@ namespace Energinet.DataHub.MeteringPoints.Tests.EDI.CreateMeteringPoint
 
             command.FromGrid.Should().Be("869");
             command.ToGrid.Should().Be("871");
-            Assert.True(command.IsOfficialAddress);
+            command.IsOfficialAddress.Should().BeNull();
             Assert.Null(command.ParentRelatedMeteringPoint);
         }
 
