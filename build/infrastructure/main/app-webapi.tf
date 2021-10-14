2fb8e5224abbb1e6c67b133cf6afc539cf46a438
Thomas Bach Andersen <xtban@energinet.dk>
2021-10-14 13:46:17 +0200

    Set dotnet version in terraform to v5 for app service

--- build/infrastructure/main/app-webapi.tf
+++ build/infrastructure/main/app-webapi.tf
@@ -18,7 +18,7 @@ resource "azurerm_app_service" "webapi" {
   app_service_plan_id = azurerm_app_service_plan.meteringpoint.id
 
   site_config {
-    dotnet_framework_version = "v4.0"
+    dotnet_framework_version = "v5.0"
     cors {
       allowed_origins = ["*"]
     }
