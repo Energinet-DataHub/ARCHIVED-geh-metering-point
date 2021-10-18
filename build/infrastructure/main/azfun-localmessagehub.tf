78acf653dd7a8e264d88efa6817b14c4e4d82221
René Nielsen <renetdev@gmail.com>
2021-10-18 12:08:40 +0200

    use shared stor postoffice

--- build/infrastructure/main/azfun-localmessagehub.tf
+++ build/infrastructure/main/azfun-localmessagehub.tf
@@ -30,9 +30,7 @@ module "azfun_localmessagehub" {
     FUNCTIONS_WORKER_RUNTIME              = "dotnet-isolated"
     # Endregion: Default Values
     METERINGPOINT_DB_CONNECTION_STRING    = local.METERING_POINT_CONNECTION_STRING
-  }
-  connection_string                         = {
-    MESSAGEHUB_STORAGE_CONNECTION_STRING  = module.stor_postoffice.primary_connection_string
+    MESSAGEHUB_STORAGE_CONNECTION_STRING  = data.azurerm_key_vault_secret.shared_resources_marketoperator_response_connection_string.value
     MESSAGEHUB_QUEUE_CONNECTION_STRING    = data.azurerm_key_vault_secret.shared_resources_integrationevents_transceiver_connection_string.value
   }
   dependencies                              = [
@@ -40,7 +38,6 @@ module "azfun_localmessagehub" {
     module.azfun_localmessagehub_plan.dependent_on,
     module.azfun_localmessagehub_stor.dependent_on,
     module.sbq_meteringpoint.dependent_on,
-    module.stor_postoffice.dependent_on,
   ]
 }
 
--- build/infrastructure/main/kv-sharedresources.tf
+++ build/infrastructure/main/kv-sharedresources.tf
@@ -40,3 +40,8 @@ data "azurerm_key_vault_secret" "shared_resources_integrationevents_transceiver_
   name         = "SHARED-RESOURCES--SB-INTEGRATIONEVENTS-TRANSCEIVER-CONNECTION-STRING"
   key_vault_id = data.azurerm_key_vault.kv_sharedresources.id
 }
+
+data "azurerm_key_vault_secret" "shared_resources_marketoperator_response_connection_string" {
+  name         = "SHARED-RESOURCES-MARKETOPERATOR-RESPONSE-CONNECTION-STRING"
+  key_vault_id = data.azurerm_key_vault.kv_sharedresources.id
+}
