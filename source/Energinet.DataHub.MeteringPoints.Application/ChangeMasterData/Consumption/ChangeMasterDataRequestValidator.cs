4fc93126f581bfa812a9f3c449ac716dbc9f197f
Kristian F. Thomsen <kft@energinet.dk>
2021-11-05 10:25:30 +0100

    Remove imports

--- source/Energinet.DataHub.MeteringPoints.Application/ChangeMasterData/Consumption/ChangeMasterDataRequestValidator.cs
+++ source/Energinet.DataHub.MeteringPoints.Application/ChangeMasterData/Consumption/ChangeMasterDataRequestValidator.cs
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.MeteringPoints.Application.Validation.Extensions;
 using Energinet.DataHub.MeteringPoints.Application.Validation.Rules;
-using Energinet.DataHub.MeteringPoints.Domain.MeteringPoints;
 using FluentValidation;
 
 namespace Energinet.DataHub.MeteringPoints.Application.ChangeMasterData.Consumption
