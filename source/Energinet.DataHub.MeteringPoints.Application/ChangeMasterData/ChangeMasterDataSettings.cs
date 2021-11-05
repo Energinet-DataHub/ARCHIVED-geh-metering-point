ee71478b83fe158d34b557900998f891cbef6359
Kristian F. Thomsen <kft@energinet.dk>
2021-11-05 11:15:05 +0100

    Change namespace

--- source/Energinet.DataHub.MeteringPoints.Application/ChangeMasterData/ChangeMasterDataSettings.cs
+++ source/Energinet.DataHub.MeteringPoints.Application/ChangeMasterData/ChangeMasterDataSettings.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.MeteringPoints.Application.ChangeMasterData.Consumption
+namespace Energinet.DataHub.MeteringPoints.Application.ChangeMasterData
 {
     public record ChangeMasterDataSettings(int NumberOfDaysEffectiveDateIsAllowedToBeforeToday = 0);
 }
--- source/Energinet.DataHub.MeteringPoints.IntegrationTests/TestHost.cs
+++ source/Energinet.DataHub.MeteringPoints.IntegrationTests/TestHost.cs
@@ -18,6 +18,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Energinet.DataHub.MeteringPoints.Application.ChangeMasterData;
 using Energinet.DataHub.MeteringPoints.Application.ChangeMasterData.Consumption;
 using Energinet.DataHub.MeteringPoints.Application.Common;
 using Energinet.DataHub.MeteringPoints.Application.Common.Commands;
