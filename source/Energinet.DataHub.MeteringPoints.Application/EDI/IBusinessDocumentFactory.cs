bb24a481d29070a6ca7ea2c2950c4166af4d749c
Christopher Maigaard <xchma@energinet.dk>
2021-11-16 16:13:15 +0100

    add missing license

--- source/Energinet.DataHub.MeteringPoints.Application/EDI/IBusinessDocumentFactory.cs
+++ source/Energinet.DataHub.MeteringPoints.Application/EDI/IBusinessDocumentFactory.cs
@@ -1,6 +1,19 @@
-﻿using Energinet.DataHub.MeteringPoints.Application.EnergySuppliers;
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.MeteringPoints.Application.EnergySuppliers;
 using Energinet.DataHub.MeteringPoints.Client.Abstractions.Models;
-using Energinet.DataHub.MeteringPoints.Domain.EnergySuppliers;
 
 namespace Energinet.DataHub.MeteringPoints.Application.EDI
 {
