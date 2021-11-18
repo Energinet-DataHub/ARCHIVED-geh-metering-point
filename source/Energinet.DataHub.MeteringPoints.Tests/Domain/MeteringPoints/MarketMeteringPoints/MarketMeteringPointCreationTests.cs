58316e15772bc3e2d68403953e6c47a725136edc
Søren Søndergaard <s.sondergaard@icloud.com>
2021-11-18 16:45:04 +0100

    POC on test inheritence
    A poc on test inheritence. The abstract class can be explored in more detail to make it seem more uniform.
    
    Perhaps another abstract method that accepts TDetails and returns a BusinessValidationResult. It still feels a bit clunky - static methods are hard....

---
+++ source/Energinet.DataHub.MeteringPoints.Tests/Domain/MeteringPoints/MarketMeteringPoints/MarketMeteringPointCreationTests.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Energinet.DataHub.MeteringPoints.Domain.MeteringPoints;
+using Energinet.DataHub.MeteringPoints.Domain.MeteringPoints.MarketMeteringPoints.Rules;
+using Energinet.DataHub.MeteringPoints.Domain.SeedWork;
+using Xunit;
+
+namespace Energinet.DataHub.MeteringPoints.Tests.Domain.MeteringPoints.MarketMeteringPoints
+{
+    public abstract class MarketMeteringPointCreationTests<TDetails> : TestBase
+        where TDetails : MeteringPointDetails
+    {
+        private readonly Func<TDetails, BusinessRulesValidationResult> _validator;
+
+        protected MarketMeteringPointCreationTests(Func<TDetails, BusinessRulesValidationResult> validator)
+        {
+            _validator = validator;
+        }
+
+        [Fact]
+        public void Should_return_error__meter_reading_occurrence_is_not_quarterly_or_hourly()
+        {
+            var details = CreateDetails()
+                with
+                {
+                    ReadingOccurrence = ReadingOccurrence.Yearly,
+                };
+
+            var result = _validator.Invoke(details);
+
+            Assert.False(result.Success);
+            AssertError<InvalidMeterReadingOccurrenceRuleError>(result, true);
+        }
+
+        protected abstract TDetails CreateDetails();
+    }
+}
