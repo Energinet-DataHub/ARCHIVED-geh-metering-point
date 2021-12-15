b3e981e0897d163302818cdc15c1b40a9893583c
Daniel Christensen <daniel@dchr.dk>
2021-12-15 11:39:13 +0100

    Fixed column name

--- source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Seed/202112141430 Actor seed data.sql
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Seed/202112141430 Actor seed data.sql
@@ -1,2 +1,2 @@
-INSERT INTO [dbo].[Actor] ([Id], [IdentificationNumber], [ActorType], [Roles])
+INSERT INTO [dbo].[Actor] ([Id], [IdentificationNumber], [IdentificationType], [Roles])
 VALUES ('158725db-35b5-4740-8ba4-80c616ec9f92', 'GLNNUMBER', 'GLN',    'MarketOperator,GridOwner')
