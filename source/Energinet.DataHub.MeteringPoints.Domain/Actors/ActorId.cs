e000a6da4b381c3c5b980586dfdde810c33b1a40
Christopher Maigaard <xchma@energinet.dk>
2021-12-16 09:54:53 +0100

    remove actorid rules

--- source/Energinet.DataHub.MeteringPoints.Domain/Actors/ActorId.cs
+++ source/Energinet.DataHub.MeteringPoints.Domain/Actors/ActorId.cs
@@ -13,8 +13,6 @@
 // limitations under the License.
 
 using System;
-using System.Collections.Generic;
-using Energinet.DataHub.MeteringPoints.Domain.Actors.Rules;
 using Energinet.DataHub.MeteringPoints.Domain.SeedWork;
 
 namespace Energinet.DataHub.MeteringPoints.Domain.Actors
