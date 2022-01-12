77705abc1636959bde83689a632dadb3bea4f2c2
Kristian F. Thomsen <kft@energinet.dk>
2022-01-12 12:31:23 +0100

    Extract method

--- source/Energinet.DataHub.MeteringPoints.Domain/MeteringPoints/MeteringPoint.cs
+++ source/Energinet.DataHub.MeteringPoints.Domain/MeteringPoints/MeteringPoint.cs
@@ -353,37 +353,7 @@ namespace Energinet.DataHub.MeteringPoints.Domain.MeteringPoints
             }
 
             _masterData = updatedMasterData ?? throw new ArgumentNullException(nameof(updatedMasterData));
-            AddDomainEvent(new MasterDataWasUpdated(
-                _masterData.Address.StreetName,
-                _masterData.Address.StreetCode,
-                _masterData.Address.City,
-                _masterData.Address.Floor,
-                _masterData.Address.Room,
-                _masterData.Address.BuildingNumber,
-                _masterData.Address.CountryCode?.Name,
-                _masterData.Address.IsActual,
-                _masterData.Address.LocationDescription,
-                _masterData.Address.MunicipalityCode,
-                _masterData.Address.PostCode,
-                _masterData.Address.CitySubDivision,
-                _masterData.Address.GeoInfoReference,
-                _masterData.Capacity?.Kw,
-                _masterData.AssetType?.Name,
-                _masterData.ConnectionType?.Name,
-                _masterData.DisconnectionType?.Name,
-                _masterData.EffectiveDate?.DateInUtc.ToString(),
-                _masterData.MeteringConfiguration.Meter.Value,
-                _masterData.MeteringConfiguration.Method.Name,
-                _masterData.PowerLimit?.Ampere,
-                _masterData.PowerLimit?.Kwh,
-                _masterData.ProductionObligation,
-                _masterData.ProductType.Name,
-                _masterData.ReadingOccurrence.Name,
-                _masterData.SettlementMethod?.Name,
-                _masterData.UnitType.Name,
-                _masterData.NetSettlementGroup?.Name,
-                _masterData.PowerPlantGsrnNumber?.Value,
-                _masterData.ScheduledMeterReadingDate?.MonthAndDay));
+            RaiseMasterDataWasUpdatedEvent();
         }
 
         internal void SetParent(MeteringPointId? parentId)
@@ -445,6 +415,41 @@ namespace Energinet.DataHub.MeteringPoints.Domain.MeteringPoints
                 _exchangeGridAreas?.TargetGridArea.Value);
 
             AddDomainEvent(@event);
+        }
+
+        private void RaiseMasterDataWasUpdatedEvent()
+        {
+            AddDomainEvent(new MasterDataWasUpdated(
+                _masterData.Address.StreetName,
+                _masterData.Address.StreetCode,
+                _masterData.Address.City,
+                _masterData.Address.Floor,
+                _masterData.Address.Room,
+                _masterData.Address.BuildingNumber,
+                _masterData.Address.CountryCode?.Name,
+                _masterData.Address.IsActual,
+                _masterData.Address.LocationDescription,
+                _masterData.Address.MunicipalityCode,
+                _masterData.Address.PostCode,
+                _masterData.Address.CitySubDivision,
+                _masterData.Address.GeoInfoReference,
+                _masterData.Capacity?.Kw,
+                _masterData.AssetType?.Name,
+                _masterData.ConnectionType?.Name,
+                _masterData.DisconnectionType?.Name,
+                _masterData.EffectiveDate?.DateInUtc.ToString(),
+                _masterData.MeteringConfiguration.Meter.Value,
+                _masterData.MeteringConfiguration.Method.Name,
+                _masterData.PowerLimit?.Ampere,
+                _masterData.PowerLimit?.Kwh,
+                _masterData.ProductionObligation,
+                _masterData.ProductType.Name,
+                _masterData.ReadingOccurrence.Name,
+                _masterData.SettlementMethod?.Name,
+                _masterData.UnitType.Name,
+                _masterData.NetSettlementGroup?.Name,
+                _masterData.PowerPlantGsrnNumber?.Value,
+                _masterData.ScheduledMeterReadingDate?.MonthAndDay));
         }
     }
 }
