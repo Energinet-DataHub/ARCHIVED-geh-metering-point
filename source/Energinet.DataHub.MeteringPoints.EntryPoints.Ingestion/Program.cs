b106c2eca8691a240b287deac98969027e947ae5
Daniel Pethjen <daniel@pethjen.dk>
2022-01-17 14:41:25 +0100

    outcomment injections

--- source/Energinet.DataHub.MeteringPoints.EntryPoints.Ingestion/Program.cs
+++ source/Energinet.DataHub.MeteringPoints.EntryPoints.Ingestion/Program.cs
@@ -18,7 +18,6 @@ using Azure.Messaging.ServiceBus;
 using Energinet.DataHub.Core.FunctionApp.Common.Abstractions.Identity;
 using Energinet.DataHub.Core.FunctionApp.Common.Identity;
 using Energinet.DataHub.Core.FunctionApp.Common.Middleware;
-using Energinet.DataHub.Core.Logging.RequestResponseMiddleware;
 using Energinet.DataHub.Core.XmlConversion.XmlConverter.Configuration;
 using Energinet.DataHub.Core.XmlConversion.XmlConverter.SimpleInjector.Extensions;
 using Energinet.DataHub.MeteringPoints.Contracts;
@@ -55,7 +54,8 @@ namespace Energinet.DataHub.MeteringPoints.EntryPoints.Ingestion
             options.UseMiddleware<JwtTokenMiddleware>();
             options.UseMiddleware<CorrelationIdMiddleware>();
             options.UseMiddleware<EntryPointTelemetryScopeMiddleware>();
-            options.UseMiddleware<RequestResponseLoggingMiddleware>();
+
+            // options.UseMiddleware<RequestResponseLoggingMiddleware>();
         }
 
         protected override void ConfigureContainer(Container container)
@@ -71,11 +71,12 @@ namespace Energinet.DataHub.MeteringPoints.EntryPoints.Ingestion
             container.Register<JwtTokenMiddleware>(Lifestyle.Scoped);
             container.Register<IUserContext, UserContext>(Lifestyle.Scoped);
             container.Register<XmlSenderValidator>(Lifestyle.Scoped);
-            container.Register<RequestResponseLoggingMiddleware>(Lifestyle.Scoped);
 
-            container.Register<IRequestResponseLogging>(() => new RequestResponseLoggingBlobStorage(
-                  Environment.GetEnvironmentVariable("REQUEST_RESPONSE_LOGGING_CONNECTION_STRING") ?? throw new InvalidOperationException(),
-                  Environment.GetEnvironmentVariable("REQUEST_RESPONSE_LOGGING_CONTAINER_NAME") ?? throw new InvalidOperationException()));
+            // container.RegisterSingleton<IRequestResponseLogging>(() => new RequestResponseLoggingBlobStorage(
+            //       Environment.GetEnvironmentVariable("REQUEST_RESPONSE_LOGGING_CONNECTION_STRING") ?? throw new InvalidOperationException(),
+                  // Environment.GetEnvironmentVariable("REQUEST_RESPONSE_LOGGING_CONTAINER_NAME") ?? throw new InvalidOperationException()));
+
+            // container.Register<RequestResponseLoggingMiddleware>(Lifestyle.Scoped);
             container.Register<MessageDispatcher, InternalDispatcher>(Lifestyle.Scoped);
             container.Register<Channel, InternalServiceBus>(Lifestyle.Scoped);
 
