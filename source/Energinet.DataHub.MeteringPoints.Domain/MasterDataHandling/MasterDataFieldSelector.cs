bda86de1e175eb590b8cefc8c733168cded31bb4
Kristian F. Thomsen <kft@energinet.dk>
2022-01-19 13:26:24 +0100

    Production obligation is ignored

--- source/Energinet.DataHub.MeteringPoints.Domain/MasterDataHandling/MasterDataFieldSelector.cs
+++ source/Energinet.DataHub.MeteringPoints.Domain/MasterDataHandling/MasterDataFieldSelector.cs
@@ -85,6 +85,7 @@ namespace Energinet.DataHub.MeteringPoints.Domain.MasterDataHandling
                 {
                     new(nameof(MasterData.ProductType), Applicability.Required),
                     new(nameof(MasterData.SettlementMethod), Applicability.NotAllowed),
+                    new(nameof(MasterData.ProductionObligation), Applicability.NotAllowed),
                 }
             },
             {
