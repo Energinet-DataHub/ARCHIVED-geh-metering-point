aff200e4b3dec6d8e2c660fd23da681cf364ac5f
Daniel Christensen <daniel@dchr.dk>
2022-01-24 13:05:13 +0100

    Added script to create user table and a many to many table between User and Actor

---
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Seed/202201241235 Add user and user relation.sql
@@ -0,0 +1,8 @@
+INSERT INTO [dbo].[User]
+VALUES ('dfc97b0f-4274-4d79-b1ab-5a9f849e33a7'),
+       ('9a264ea6-1eb6-412b-b8f3-9234f3d29726');
+
+INSERT INTO [dbo].[UserActor]
+VALUES ('dfc97b0f-4274-4d79-b1ab-5a9f849e33a7', '6b5a4b65-5368-4895-b440-85a22588e299'),
+       ('dfc97b0f-4274-4d79-b1ab-5a9f849e33a7', '76f4e62b-eb01-44cb-937b-dac9dd0cefff'),
+       ('9a264ea6-1eb6-412b-b8f3-9234f3d29726', '76f4e62b-eb01-44cb-937b-dac9dd0cefff')
