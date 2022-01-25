07f51e601e700548a179b9d22a72de1cbbc533ee
Daniel Christensen <daniel@dchr.dk>
2022-01-25 14:51:12 +0100

    Added message hub test actor user

---
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Seed/202201251443 Updates from actor registry.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Actor] ([Id],[IdentificationNumber],[IdentificationType],[Roles]) VALUES ('a982de1f-3703-4e3c-be56-4e54418a2a59','0808118335003','GLN','Foo')
+
+INSERT INTO [dbo].[GridAreas]([Id],[Code],[Name],[PriceAreaCode],[FullFlexFromDate],[ActorId]) VALUES ('7ce2d3e1-2fac-49e3-bf46-de75c5f8b714','112','Batman Message hub test user','DK1',null,'a982de1f-3703-4e3c-be56-4e54418a2a59')
