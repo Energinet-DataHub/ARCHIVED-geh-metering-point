03922445a6d5397fa40335306b6890f68803e8f1
Kristian F. Thomsen <kft@energinet.dk>
2022-01-25 15:56:05 +0100

    Remove unused import

--- source/Energinet.DataHub.MeteringPoints.Application/ChangeMasterData/ChangeMasterDataHandler.cs
+++ source/Energinet.DataHub.MeteringPoints.Application/ChangeMasterData/ChangeMasterDataHandler.cs
@@ -21,7 +21,6 @@ using Energinet.DataHub.MeteringPoints.Application.Common;
 using Energinet.DataHub.MeteringPoints.Application.Common.ChildMeteringPoints;
 using Energinet.DataHub.MeteringPoints.Application.Validation.ValidationErrors;
 using Energinet.DataHub.MeteringPoints.Domain.MasterDataHandling;
-using Energinet.DataHub.MeteringPoints.Domain.MasterDataHandling.Components.Addresses;
 using Energinet.DataHub.MeteringPoints.Domain.MeteringPoints;
 using Energinet.DataHub.MeteringPoints.Domain.Policies;
 using Energinet.DataHub.MeteringPoints.Domain.SeedWork;
