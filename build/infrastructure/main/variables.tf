20aaeb050392f7b7a5ee27c87caa86dd162f9d77
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-02-11 12:50:05 +0100

    subnet address spaces are injected using variables

--- build/infrastructure/main/variables.tf
+++ build/infrastructure/main/variables.tf
@@ -45,3 +45,23 @@ variable shared_resources_resource_group_name {
   type          = string
   description   = "Name of the Resource Group, that contains the shared resources."
 }
+
+variable "subnet_internal_private_endpoints_address_space" {
+  type          = string
+  description   = "Address space of the subnet for internal private endpoints"
+}
+
+variable "subnet_external_private_endpoints_address_space" {
+  type          = string
+  description   = "Address space of the subnet for external private endpoints"
+}
+
+variable "subnet_vnet_integrations_webapi_address_space" {
+  type          = string
+  description   = "Address space of the subnet for Virtual Network Integrations of WebAPIs"
+}
+
+variable "subnet_vnet_integrations_functions_address_space" {
+  type          = string
+  description   = "Address space of the subnet for Virtual Network Integrations of Azure Functions"
+}
