e14630389ac9f528a2e3212cb358197a5ff70772
Chris Brohus <CBS@energinet.dk>
2022-02-16 10:03:07 +0100

    Deleted MeterNumberNotAllowedErrorConverter

--- source/Energinet.DataHub.MeteringPoints.Tests/Domain/MeteringConfigurationTests.cs
+++ source/Energinet.DataHub.MeteringPoints.Tests/Domain/MeteringConfigurationTests.cs
@@ -32,7 +32,7 @@ namespace Energinet.DataHub.MeteringPoints.Tests.Domain
         {
             var result = MeteringConfiguration.CheckRules(MeteringMethod.Virtual, MeterId.Create("fakeId"));
 
-            AssertError<MeterIdIsNotAllowedRuleError>(result, true);
+            AssertError<MeterIdIsNotAllowedRuleError>("E86", result, true);
         }
 
         [Fact]
@@ -40,7 +40,7 @@ namespace Energinet.DataHub.MeteringPoints.Tests.Domain
         {
             var result = MeteringConfiguration.CheckRules(MeteringMethod.Calculated, MeterId.Create("fakeId"));
 
-            AssertError<MeterIdIsNotAllowedRuleError>(result, true);
+            AssertError<MeterIdIsNotAllowedRuleError>("E86", result, true);
         }
 
         [Fact]
