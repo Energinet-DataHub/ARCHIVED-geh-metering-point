a8bc6b3c0c669fdf5d6b67168594ca009163ebbb
Christopher Maigaard <xchma@energinet.dk>
2022-02-23 09:22:29 +0100

    add entrypoint based test host poc

---
+++ source/Energinet.DataHub.MeteringPoints.IntegrationTests/CreateMeteringPoints/CreateFooTests.cs
@@ -0,0 +1,85 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Energinet.DataHub.Core.App.Common;
+using Energinet.DataHub.Core.App.Common.Abstractions.Actor;
+using Energinet.DataHub.Core.App.Common.Abstractions.Users;
+using Energinet.DataHub.MeteringPoints.Application.Create;
+using Energinet.DataHub.MeteringPoints.Client.Abstractions.Models;
+using Energinet.DataHub.MeteringPoints.EntryPoints.Processing;
+using Energinet.DataHub.MeteringPoints.EntryPoints.WebApi.MeteringPoints.Queries;
+using Energinet.DataHub.MeteringPoints.Infrastructure.Correlation;
+using Energinet.DataHub.MeteringPoints.IntegrationTests.Tooling;
+using MediatR;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using SimpleInjector;
+using Xunit;
+using Xunit.Categories;
+
+namespace Energinet.DataHub.MeteringPoints.IntegrationTests.CreateMeteringPoints
+{
+    [IntegrationTest]
+    public class CreateFooTests
+        : EntryPointBasedFixture<Program>
+    {
+        public CreateFooTests(DatabaseFixture databaseFixture)
+            : base(databaseFixture) { }
+
+        [Fact]
+        public async Task Metering_point_created_shows_in_process_overview()
+        {
+            var request = CreateCommand();
+
+            await SendCommandAsync(request).ConfigureAwait(false);
+
+            await AssertProcessOverviewAsync(
+                    SampleData.GsrnNumber,
+                    "BRS-004",
+                    "RequestCreateMeteringPoint",
+                    "ConfirmCreateMeteringPoint")
+                .ConfigureAwait(false);
+        }
+
+        protected override void AddEnvironmentVariables([NotNull] Dictionary<string, string> variables)
+        {
+            variables.Add("METERINGPOINT_DB_CONNECTION_STRING", DatabaseConnectionString);
+        }
+
+        protected override void OverrideRegistrations([NotNull] Container container)
+        {
+            // Fake current actor and user
+            container.Register<IActorContext>(() => new ActorContext { CurrentActor = new Actor(SampleData.GridOperatorIdOfGrid870, "GLN", "8200000001409", "GridAccessProvider") }, Lifestyle.Singleton);
+            container.Register<IUserContext>(() => new UserContext { CurrentUser = new User(Guid.NewGuid(), new List<Guid> { Guid.NewGuid() }) }, Lifestyle.Singleton);
+
+            // Singleton for tests
+            container.Register<ICorrelationContext, CorrelationContext>(Lifestyle.Singleton);
+
+            // Specific for test instead of using Application Insights package
+            container.Register(() => new TelemetryClient(new TelemetryConfiguration()), Lifestyle.Scoped);
+
+            // Only for asserting process overview creation
+            container.Register<IRequestHandler<MeteringPointProcessesByGsrnQuery, List<Process>>, MeteringPointProcessesByGsrnQueryHandler>();
+        }
+
+        private static CreateMeteringPoint CreateCommand()
+        {
+            return Scenarios.CreateConsumptionMeteringPointCommand();
+        }
+    }
+}
---
+++ source/Energinet.DataHub.MeteringPoints.IntegrationTests/Tooling/EntryPointBasedFixture.cs
@@ -0,0 +1,163 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.MeteringPoints.Client.Abstractions.Models;
+using Energinet.DataHub.MeteringPoints.EntryPoints.Common;
+using Energinet.DataHub.MeteringPoints.EntryPoints.WebApi.MeteringPoints.Queries;
+using Energinet.DataHub.MeteringPoints.Infrastructure.Correlation;
+using Energinet.DataHub.MeteringPoints.Infrastructure.DataAccess;
+using FluentAssertions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using Xunit;
+using Xunit.Categories;
+
+namespace Energinet.DataHub.MeteringPoints.IntegrationTests.Tooling
+{
+    [Collection("IntegrationTest")]
+    [IntegrationTest]
+    public class EntryPointBasedFixture<TEntryPoint> : IDisposable
+        where TEntryPoint : EntryPoint, new()
+    {
+        private readonly Container _container;
+        private readonly Scope _scope;
+        private bool _disposed;
+
+        public EntryPointBasedFixture(
+            DatabaseFixture databaseFixture)
+        {
+            if (databaseFixture == null) throw new ArgumentNullException(nameof(databaseFixture));
+            databaseFixture.DatabaseManager.UpgradeDatabase();
+            DatabaseConnectionString = databaseFixture.DatabaseManager.ConnectionString;
+
+            SetEnvironmentVariables();
+
+            _container = new Container();
+            InitializeContainer(_container);
+
+            OverrideRegistrations();
+
+            _container.Verify();
+            _scope = AsyncScopedLifestyle.BeginScope(_container);
+            _container
+                .GetInstance<ICorrelationContext>()
+                .SetId(Guid.NewGuid().ToString().Replace("-", string.Empty, StringComparison.Ordinal));
+        }
+
+        protected string DatabaseConnectionString { get; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        internal TService GetService<TService>()
+            where TService : class
+        {
+            return _container.GetInstance<TService>();
+        }
+
+        protected virtual void AddEnvironmentVariables([NotNull] Dictionary<string, string> variables) { }
+
+        protected virtual void OverrideRegistrations([NotNull] Container container) { }
+
+        protected async Task SendCommandAsync(object command, CancellationToken cancellationToken = default)
+        {
+            await using var scope = AsyncScopedLifestyle.BeginScope(_container);
+            await scope.GetInstance<IMediator>().Send(command, cancellationToken).ConfigureAwait(false);
+        }
+
+        protected async Task AssertProcessOverviewAsync(
+            string gsrn,
+            string expectedProcessName,
+            params string[] expectedProcessSteps)
+        {
+            var processes = await _container.GetInstance<IRequestHandler<MeteringPointProcessesByGsrnQuery, List<Process>>>()
+                .Handle(new MeteringPointProcessesByGsrnQuery(gsrn), CancellationToken.None)
+                .ConfigureAwait(false);
+
+            processes.Should().ContainSingle(process => process.Name == expectedProcessName, $"a single process with name {expectedProcessName} was expected")
+                .Which.Details.Select(detail => detail.Name).Should().ContainInOrder(expectedProcessSteps);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed || disposing)
+            {
+                return;
+            }
+
+            CleanupDatabase();
+            _scope.Dispose();
+            _container.Dispose();
+            _disposed = true;
+        }
+
+        private static void InitializeContainer(Container container)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            var entryPoint = new TEntryPoint();
+            var containerField = typeof(EntryPoint).GetField(
+                "_container",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            containerField?.SetValue(entryPoint, container);
+            entryPoint.ConfigureApplication();
+
+            container.Options.AllowOverridingRegistrations = true;
+        }
+
+        private void SetEnvironmentVariables()
+        {
+            var variables = new Dictionary<string, string>();
+            AddEnvironmentVariables(variables);
+            foreach (var variable in variables)
+            {
+                Environment.SetEnvironmentVariable(variable.Key, variable.Value);
+            }
+        }
+
+        private void OverrideRegistrations()
+        {
+            OverrideRegistrations(_container);
+        }
+
+        private void CleanupDatabase()
+        {
+            var cleanupStatement = new StringBuilder();
+
+            cleanupStatement.AppendLine("DELETE FROM EnergySuppliers");
+            cleanupStatement.AppendLine("DELETE FROM MeteringPoints");
+            cleanupStatement.AppendLine("DELETE FROM OutboxMessages");
+            cleanupStatement.AppendLine("DELETE FROM QueuedInternalCommands");
+            cleanupStatement.AppendLine("DELETE FROM GridAreaLinks");
+            cleanupStatement.AppendLine("DELETE FROM GridAreas");
+            cleanupStatement.AppendLine("DELETE FROM MessageHubMessages");
+            cleanupStatement.AppendLine("DELETE FROM BusinessProcesses");
+
+            _container.GetInstance<MeteringPointContext>()
+                .Database.ExecuteSqlRaw(cleanupStatement.ToString());
+        }
+    }
+}
