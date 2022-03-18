ddad1c183081d0c79e451601e10c8ee6f2b8066d
Daniel Christensen <daniel@dchr.dk>
2022-03-18 12:51:23 +0100

    Fixed type

--- source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Seed/202203181138 Update test actor.sql
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Seed/202203181138 Update test actor.sql
@@ -1,8 +1,8 @@
 -- Updated DDM 7 Actor with ID from t-001 instead of u-001
 
 
-declare @newActorId varchar = '55878BD4-EF25-4F2E-8BA8-B2F3CCA962E6';
-declare @oldActorId varchar = 'CDDF2D12-3F02-4A52-B1C1-C0D4E03262C4';
+declare @newActorId UNIQUEIDENTIFIER = '55878BD4-EF25-4F2E-8BA8-B2F3CCA962E6';
+declare @oldActorId UNIQUEIDENTIFIER = 'CDDF2D12-3F02-4A52-B1C1-C0D4E03262C4';
 
 INSERT INTO [dbo].Actor (Id, IdentificationNumber, IdentificationType, Roles)
 VALUES (@newActorId, 8200000002727, 'GLN', 'GridAccessProvider,MeteredDataResponsible');
