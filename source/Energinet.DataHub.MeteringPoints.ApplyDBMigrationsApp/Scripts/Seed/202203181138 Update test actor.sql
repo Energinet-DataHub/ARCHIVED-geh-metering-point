4ad384b02bc0cf91c65b04e2fcc88181c1089c3b
Daniel Christensen <daniel@dchr.dk>
2022-03-18 12:34:43 +0100

    Thanks, SonarCloud..

--- source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Seed/202203181138 Update test actor.sql
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Seed/202203181138 Update test actor.sql
@@ -1,10 +1,16 @@
 -- Updated DDM 7 Actor with ID from t-001 instead of u-001
 
+
+declare @newActorId varchar = '55878BD4-EF25-4F2E-8BA8-B2F3CCA962E6';
+declare @oldActorId varchar = 'CDDF2D12-3F02-4A52-B1C1-C0D4E03262C4';
+
 INSERT INTO [dbo].Actor (Id, IdentificationNumber, IdentificationType, Roles)
-VALUES ('55878BD4-EF25-4F2E-8BA8-B2F3CCA962E6',8200000002727,'GLN', 'GridAccessProvider,MeteredDataResponsible');
+VALUES (@newActorId, 8200000002727, 'GLN', 'GridAccessProvider,MeteredDataResponsible');
 
-UPDATE [dbo].UserActor SET ActorId = '55878BD4-EF25-4F2E-8BA8-B2F3CCA962E6' WHERE ActorId = 'CDDF2D12-3F02-4A52-B1C1-C0D4E03262C4';
+UPDATE [dbo].UserActor SET ActorId = @newActorId
+WHERE ActorId = @oldActorId;
 
-UPDATE [dbo].GridAreas SET ActorId = '55878BD4-EF25-4F2E-8BA8-B2F3CCA962E6' WHERE ActorId = 'CDDF2D12-3F02-4A52-B1C1-C0D4E03262C4';
+UPDATE [dbo].GridAreas SET ActorId = @newActorId
+WHERE ActorId = @oldActorId;
 
-DELETE FROM [dbo].Actor WHERE Id = 'CDDF2D12-3F02-4A52-B1C1-C0D4E03262C4';
+DELETE FROM [dbo].Actor WHERE Id = @oldActorId;
