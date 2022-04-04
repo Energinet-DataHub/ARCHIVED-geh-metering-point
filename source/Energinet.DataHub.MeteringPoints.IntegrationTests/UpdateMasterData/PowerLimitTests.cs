514d92c8922887a0e87cc6559a8665d8502b382a
Daniel Pethjen <daniel@pethjen.dk>
2022-04-04 12:08:46 +0200

    make Power limit nullable

--- source/Energinet.DataHub.MeteringPoints.IntegrationTests/UpdateMasterData/PowerLimitTests.cs
+++ source/Energinet.DataHub.MeteringPoints.IntegrationTests/UpdateMasterData/PowerLimitTests.cs
@@ -50,6 +50,28 @@ namespace Energinet.DataHub.MeteringPoints.IntegrationTests.UpdateMasterData
         }
 
         [Fact]
+        public async Task Power_limit_is_changed_if_null()
+        {
+            await SendCommandAsync(Scenarios.CreateVEProduction() with
+            {
+                MaximumCurrent = null,
+                MaximumPower = null,
+            }).ConfigureAwait(false);
+
+            var request = CreateUpdateRequest()
+                with
+                {
+                    MaximumCurrent = "2",
+                    MaximumPower = string.Empty,
+                };
+
+            await SendCommandAsync(request).ConfigureAwait(false);
+
+            AssertMasterData()
+                .HasPowerLimit(null, 2);
+        }
+
+        [Fact]
         public async Task Reject_if_max_current_input_is_invalid()
         {
             await SendCommandAsync(Scenarios.CreateVEProduction() with
