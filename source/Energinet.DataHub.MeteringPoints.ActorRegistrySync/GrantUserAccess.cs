ac31c1f49a4355d585644c73c7a0dfb6c8974d75
Daniel Christensen <daniel@dchr.dk>
2022-06-07 11:49:30 +0200

    Set up HTTP trigger to insert user object

---
+++ source/Energinet.DataHub.MeteringPoints.ActorRegistrySync/GrantUserAccess.cs
@@ -0,0 +1,94 @@
+﻿// Copyright 2022 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Energinet.DataHub.MeteringPoints.ActorRegistrySync.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Energinet.DataHub.MeteringPoints.ActorRegistrySync;
+
+public static class GrantUserAccess
+{
+    [FunctionName("GrantUserAccess")]
+    public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log)
+    {
+        if (req == null) throw new ArgumentNullException(nameof(req));
+        if (log == null) throw new ArgumentNullException(nameof(log));
+
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        using var streamReader = new StreamReader(req.Body);
+        var requestBody = await streamReader.ReadToEndAsync().ConfigureAwait(false);
+        var data = JsonConvert.DeserializeObject<UserActorDto>(requestBody);
+
+        if (data == null)
+        {
+            return new BadRequestObjectResult("Data missing");
+        }
+
+        var userObjectId = new Guid(data.UserObjectId);
+
+        var meteringPointConnectionString = Environment.GetEnvironmentVariable("METERINGPOINT_DB_CONNECTION_STRING");
+        using var meteringPointSqlConnection = new SqlConnection(meteringPointConnectionString);
+
+        var actorIds = (await GetActorIdsByGlnNumbersAsync(meteringPointSqlConnection, data.GlnNumbers).ConfigureAwait(false)).ToList();
+
+        var existingPermissions = await GetExistingActorIdsAsync(meteringPointSqlConnection, userObjectId, actorIds).ConfigureAwait(false);
+
+        var remainingActorIds = actorIds.Where(id => !existingPermissions.Contains(id)).ToList();
+
+        var userActorParams = remainingActorIds.Select(actorId => new UserActorParam(userObjectId, actorId));
+
+        await UpdateUserActorPermissionsAsync(meteringPointSqlConnection, userActorParams).ConfigureAwait(false);
+
+        return new OkObjectResult($"Hello, {userObjectId}");
+    }
+
+    private static async Task<IEnumerable<Guid>> GetExistingActorIdsAsync(SqlConnection sqlConnection, Guid userObjectId, IEnumerable<Guid> actorIds)
+    {
+        return await sqlConnection.QueryAsync<Guid>(
+            @"SELECT ActorId FROM [dbo].[UserActor]
+               WHERE UserId = @userObjectId
+               AND ActorId IN @actorIds",
+            new { userObjectId, actorIds }).ConfigureAwait(false);
+    }
+
+    private static async Task UpdateUserActorPermissionsAsync(IDbConnection sqlConnection, IEnumerable<UserActorParam> userActorParams)
+    {
+        var insertQuery = "INSERT INTO [dbo].[UserActor] (UserId, ActorId) VALUES (@UserId, @ActorId)";
+        await sqlConnection.ExecuteAsync(insertQuery, userActorParams).ConfigureAwait(false);
+    }
+
+    private static async Task<IEnumerable<Guid>> GetActorIdsByGlnNumbersAsync(IDbConnection sqlConnection, IReadOnlyCollection<string> glnNumbers)
+    {
+        return await sqlConnection.QueryAsync<Guid>(
+            @"SELECT Id
+                   FROM [dbo].[Actor] WHERE Actor.IdentificationNumber IN @glnNumbers",
+            new { glnNumbers }).ConfigureAwait(false);
+    }
+}
+
+internal record UserActorParam(Guid UserId, Guid ActorId);
