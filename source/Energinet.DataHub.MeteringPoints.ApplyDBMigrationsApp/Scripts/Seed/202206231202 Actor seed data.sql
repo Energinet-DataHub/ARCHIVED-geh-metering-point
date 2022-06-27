cc5d25e51ca0a46ac9f436063a1fe44adaa6d360
Daniel Christensen <daniel@dchr.dk>
2022-06-27 12:46:09 +0200

    Added actor and grid area seed data

---
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Seed/202206231202 Actor seed data.sql
@@ -0,0 +1,17 @@
+INSERT INTO [dbo].[Actor] ([Id], [IdentificationNumber], [IdentificationType], [Roles])
+VALUES ('08e2ba01-0ead-48c0-bdc7-8e2c7f7c5525', '5790001330552', 'GLN', 'MeteringPointAdministrator'),
+       ('158725db-35b5-4740-8ba4-80c616ec9f92', '8200000001409', 'GLN', 'GridAccessProvider'),
+       ('b495f493-40db-41c4-a43c-1569d0024307', '8200000005391', 'GLN', 'GridAccessProvider'),
+       ('95a2890e-549f-4e75-bac2-a19b56129e46', '8200000009665', 'GLN', 'GridAccessProvider'),
+       ('9e61de1f-7495-4072-aaec-0a127f1f396d', '8200000003014', 'GLN', 'GridAccessProvider'),
+       ('edde4360-a92a-423f-907b-31fd1d1135c1', '8200000008828', 'GLN', 'GridAccessProvider'),
+       ('7e4be814-a278-4100-95cf-930c385cc62e', '8200000000501', 'GLN', 'GridAccessProvider'),
+       ('cb39ffdb-58a0-4363-8b14-47bb805b29ab', '8200000003090', 'GLN', 'GridAccessProvider'),
+       ('801e4657-3375-4a2e-a0dd-ed1f7ac377f2', '8200000007005', 'GLN', 'GridAccessProvider'),
+       ('76f4e62b-eb01-44cb-937b-dac9dd0cefff', '8200000000099', 'GLN', 'GridAccessProvider'),
+       ('6b5a4b65-5368-4895-b440-85a22588e299', '8200000009139', 'GLN', 'GridAccessProvider'),
+       ('e9365635-bae6-4da3-aeee-100f55f2116d', '8200000003939', 'GLN', 'GridAccessProvider'),
+       ('f0cc5ecf-3401-4714-a1a1-1bfc3ce61f3d', '8100000000016', 'GLN', 'GridAccessProvider'),
+       ('a117349a-0265-4ce9-9303-a4f84bf0eb9c', '8100000000023', 'GLN', 'GridAccessProvider'),
+       ('31e06e54-3473-4f3d-94d3-c91f26dcd092', '8100000000030', 'GLN', 'GridAccessProvider'),
+       ('e9365635-bae6-4da3-aeee-100f55f2116d', '8200000003939', 'GLN', 'GridAccessProvider')
