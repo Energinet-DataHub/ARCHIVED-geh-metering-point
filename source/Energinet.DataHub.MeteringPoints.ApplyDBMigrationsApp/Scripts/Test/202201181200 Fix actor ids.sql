7c864e38922f09cb8e2f2bc14ed4f2e97a2ab526
Daniel Christensen <daniel@dchr.dk>
2022-06-27 17:09:00 +0200

    Re-added test data

--- source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Test/202201181200 Fix actor ids.sql
+++ source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Test/202201181200 Fix actor ids.sql
@@ -1,6 +1,3 @@
-INSERT INTO [dbo].[Actor] ([Id], [IdentificationNumber], [IdentificationType], [Roles])
-VALUES ('90f6f4e5-8073-4842-b33e-c59e9f4a8c3f', '8200000001409', 'GLN',    'GridAccessProvider')
-
 UPDATE [dbo].[GridAreas]
 SET [ActorId] = '90f6f4e5-8073-4842-b33e-c59e9f4a8c3f'
 WHERE [ActorId] = '158725db-35b5-4740-8ba4-80c616ec9f92'
