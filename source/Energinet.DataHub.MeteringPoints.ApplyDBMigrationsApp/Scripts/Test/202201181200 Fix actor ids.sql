233b035acc86f42de2ddf815a18280fd3b63bfe9
Kristian F. Thomsen <kft@energinet.dk>
2022-09-15 09:25:59 +0200

    Remove script

--- source/Energinet.DataHub.MeteringPoints.ApplyDBMigrationsApp/Scripts/Test/202201181200 Fix actor ids.sql
+++
@@ -1,6 +0,0 @@
-UPDATE [dbo].[GridAreas]
-SET [ActorId] = '90f6f4e5-8073-4842-b33e-c59e9f4a8c3f'
-WHERE [ActorId] = '158725db-35b5-4740-8ba4-80c616ec9f92'
-
-DELETE FROM [dbo].[Actor]
-WHERE [Id] = '158725db-35b5-4740-8ba4-80c616ec9f92'
