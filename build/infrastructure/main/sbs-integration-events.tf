f0c8e6f7ce9691264342d17f8f3e77e4691dae1b
Daniel Pethjen <daniel@pethjen.dk>
2022-09-23 13:36:31 +0200

    Add mp message dequeeud listener

--- build/infrastructure/main/sbs-integration-events.tf
+++ build/infrastructure/main/sbs-integration-events.tf
@@ -23,3 +23,16 @@ module "sbs_energy_supplier_changed" {
     }  
   }
 }
+
+module "sbs_metering_point_message_dequeued" {
+  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic-subscription?ref=v9"
+  name                = "sbs-metering-point-message-dequeued"
+  topic_id            = data.azurerm_key_vault_secret.sbt_domainrelay_integrationevent_received_name_id.value
+  project_name        = var.domain_name_short
+  max_delivery_count  = 10 
+  correlation_filter  = {
+    properties     = {
+      "MessageType" = "MeteringPointMessageDequeuedIntegrationEvent",
+    }  
+  }
+}
